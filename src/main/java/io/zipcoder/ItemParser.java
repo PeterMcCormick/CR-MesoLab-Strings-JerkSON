02915b426f4a26869eff8942f074ba87a0cc56b6
PeterMcCormick <mcpeter@udel.edu>
2018-03-17 14:02:56 -0400

    Pausin

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -1,9 +1,6 @@
 package io.zipcoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +8,9 @@ public class ItemParser {
 
     Pattern pattern;
     Matcher matcher;
+    Integer countExceptionsThrown = 0;
+
+
 
     private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString){
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
@@ -22,30 +22,26 @@ public class ItemParser {
         return response;
     }
 
-    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        int count = 0;
+    public Integer getExceptionsThrown(String rawData)  {
 
-        ArrayList<Item> myItem = new ArrayList<>();
-
-        String name = "(N|n)..(E|e)";
-        String strPrice = "\\d\\.\\d\\d";
-        Double price = Double.valueOf(strPrice);
-        String type = "(T|t)..(E|e)";
-        String expiration = "(E|e)........(N|n)";
-        ArrayList<String> temp = parseRawDataIntoStringArray(rawItem);
-
-        for (int i = 0; i < temp.size(); i++) {
-            if (findKeyValuePairsInRawItemData(temp.get(i)).equals(null))  {
-                count++;
-                throw new ItemParseException();
-            }
-            else if (findKeyValuePairsInRawItemData(temp.get(i)).size() == 4) {
-                for (int j = 0; j < temp.size() ; j++) {
-                    findKeyValuePairsInRawItemData(temp.get(i));
-                }
-            }
+        ArrayList<String> parsedRawData = parseRawDataIntoStringArray(rawData);
+        for (int i = 0; i < parsedRawData.size(); i++)
+            try {
+            parseStringIntoItem(parsedRawData.get(i));
+            } catch (ItemParseException e){
         }
-        return new Item (name, price, type, expiration);
+        return this.countExceptionsThrown;
+    }
+
+    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
+
+        String name = checkName(rawItem);
+        Double price = Double.valueOf(checkPrice(rawItem));
+        String type = checkType(rawItem);
+        String expiration = checkExpiration(rawItem);
+
+        return new Item(name, price, type, expiration);
+
     }
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
         String stringPattern = "[@|;|^|%|*|%]";
@@ -53,17 +49,92 @@ public class ItemParser {
         return response;
     }
 
-    public Object checkSpelling() {
-        return null;
+    public String fixCookie(String input) {
+        Pattern patternCookies = Pattern.compile("[Cc][oO0][oO0][kK][iI][eE][sS]");
+        Matcher matcherCookie = patternCookies.matcher(input);
+        return matcherCookie.replaceAll("cookies");
     }
 
-    public Object checkName(String nameInput){
-        String regexName = "(N|n)..(E|e)";
-        Pattern patternName = Pattern.compile(regexName);
-        pattern = patternName.compile(regexName);
-        Matcher matcherName = patternName.matcher(nameInput);
-        return matcher.replaceAll("Name");
+    public String checkName(String input) throws ItemParseException{
+
+        String newInput = fixCookie(input);
+        Pattern patternName = Pattern.compile("([Nn]..[Ee]:)(\\w+)");
+        Matcher matcherName = patternName.matcher(input);
+
+        if(matcherName.find())
+            return matcherName.group(2).toLowerCase();
+        else {
+            countExceptionsThrown++;
+            throw new ItemParseException();
+        }
 
     }
+
+    public String checkPrice(String input) throws ItemParseException{
+        Pattern patternPrice = Pattern.compile("([Pp]...[Ee]:)(\\d\\.\\d{2})");
+        Matcher matcherPrice = patternPrice.matcher(input);
+
+        if(matcherPrice.find())
+            return matcherPrice.group(2);
+        else {
+            countExceptionsThrown++;
+            throw new ItemParseException();
+        }
+    }
+
+
+    public String checkType(String input) throws ItemParseException {
+        Pattern patternType = Pattern.compile("([Tt]..[Ee]:)(\\w+)");
+        Matcher matcherType = patternType.matcher(input);
+
+        if(matcherType.find())
+            return matcherType.group(2).toLowerCase();
+        else {
+            countExceptionsThrown++;
+            throw new ItemParseException();
+        }
+    }
+
+    public String checkExpiration(String input) throws ItemParseException {
+        Pattern patternExpiration = Pattern.compile("([Ee]........[Nn]:)(\\d\\/\\d{2}\\/\\d{4})");
+        Matcher matcherExpiration = patternExpiration.matcher(input);
+
+        if(matcherExpiration.find())
+            return matcherExpiration.group(2);
+        else {
+            countExceptionsThrown++;
+            throw new ItemParseException();
+        }
+
+    }
+
+
 
 }
+
+
+
+
+//    int count = 0;
+//
+//    ArrayList<Item> myItem = new ArrayList<>();
+//
+//    String name = "(N|n)..(E|e)";
+//    String strPrice = "\\d\\.\\d\\d";
+//    Double price = Double.valueOf(strPrice);
+//    String type = "(T|t)..(E|e)";
+//    String expiration = "(E|e)........(N|n)";
+//    ArrayList<String> temp = parseRawDataIntoStringArray(rawItem);
+//
+//        for (int i = 0; i < temp.size(); i++) {
+//        if (findKeyValuePairsInRawItemData(temp.get(i)).equals(null))  {
+//        count++;
+//        throw new ItemParseException();
+//        }
+//        else if (findKeyValuePairsInRawItemData(temp.get(i)).size() == 4) {
+//        for (int j = 0; j < temp.size() ; j++) {
+//        findKeyValuePairsInRawItemData(temp.get(i));
+//        }
+//        }
+//        }
+//        return new Item (name, price, type, expiration);
--- src/main/java/io/zipcoder/Main.java
+++ src/main/java/io/zipcoder/Main.java
@@ -2,8 +2,15 @@ package io.zipcoder;
 
 import org.apache.commons.io.IOUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 
 public class Main {
+
+    public HashMap<String, ArrayList<Double>> namePrice;
+    HashMap<HashMap<String, ArrayList<Double>>, Integer> namePriceOccurence = new HashMap<>();
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -15,5 +22,17 @@ public class Main {
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
         // TODO: parse the data in output into items, and display to console.
+
+
+    }
+
+    public HashMap getNamePrice(String name, Double price) {
+
+        if (!namePrice.containsKey(name)) {
+            namePrice.put(name, new ArrayList<>());
+            namePrice.get(name).add(price);
+        } else {
+            namePriceOccurence.put(namePriceOccurence.containsKey(;
+        }
     }
 }
